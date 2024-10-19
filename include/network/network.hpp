b92e53bc05ac5a81fabe7e1df2c7ed91117d80ae
Valiance <lucas.ludovic@epitech.eu>
2024-10-19 22:28:21 +0200

    change: (network) use a class instead of a static class

--- include/network/network.hpp
+++ include/network/network.hpp
@@ -1,16 +1,22 @@
 #ifndef NETWORK_HPP_
 #define NETWORK_HPP_
 
+#include <boost/asio/io_service.hpp>
 #include <string>
 
 class Network
 {
   public:
-    static int send_data(const std::string& ip_receiver,
-                         const std::string& data);
-    static int retrieve_send_datas();
-    static int convert_string_to_send_data(const std::string& data);
-    static int convert_send_data_to_string(const std::string& data);
+    Network();
+    ~Network();
+    int send_data(const std::string& ip_receiver, const std::string& data);
+    int retrieve_send_datas();
+
+  private:
+    boost::asio::io_service io_service;
+
+    int convert_string_to_send_data(const std::string& data);
+    std::string convert_received_data_to_string(const int data);
 };
 
 #endif
--- src/network/network.cpp
+++ src/network/network.cpp
@@ -1,10 +1,34 @@
 #include "network/network.hpp"
 #include "my_macros.hpp"
+#include <boost/asio.hpp>
+#include <boost/asio/io_service.hpp>
+
+Network::Network() {}
+
+Network::~Network() {}
 
 int
 Network::send_data(const std::string& ip_receiver, const std::string& data)
 {
-    int data_to_send = Network::convert_string_to_send_data(data);
+    int data_to_send = this->convert_string_to_send_data(data);
 
     return SUCCESS;
+}
+
+int
+Network::retrieve_send_datas()
+{
+    return SUCCESS;
+}
+
+int
+Network::convert_string_to_send_data(const std::string& data)
+{
+    return 0;
+}
+
+std::string
+Network::convert_received_data_to_string(const int received_data)
+{
+    return "haha";
 }
